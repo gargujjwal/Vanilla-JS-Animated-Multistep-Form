8adb82271177e01a6b4373a7d0d409f7aa823016
Ujjwal Garg <ujjwalgarg100204@gmail.com>
2023-06-04 14:40:30 +0530

    make it responsive

--- app.js
+++ app.js
@@ -3,32 +3,39 @@
 const formCounter = Array.from(document.querySelectorAll(".steps > li"));
 const formSteps = Array.from(document.querySelectorAll("[data-step-number]"));
 
-let currStep = formSteps.find(step => step.classList.contains("active")).getAttribute("data-step-number");
+let currStep = formSteps
+	.find(step => step.classList.contains("active"))
+	.getAttribute("data-step-number");
 
-document.querySelectorAll(`.next`)
-    .forEach(btn => btn.addEventListener("click", () => {
-        formSteps[currStep].classList.toggle("slide-left");
-        setTimeout(() => {
-            formSteps[currStep].classList.toggle("slide-left");
-            formSteps[currStep].classList.toggle("active");
-            currStep = (currStep + 1) % 3;
-            formSteps[currStep].classList.toggle("active");
-            formCounter[currStep].classList.toggle("active");
+document.querySelector("form").addEventListener("submit", ev => {
+	ev.preventDefault();
+	document.querySelector(".next").click();
+});
 
-        }, 600)
-    }));
+document.querySelectorAll(`.next`).forEach(btn =>
+	btn.addEventListener("click", () => {
+		formSteps[currStep].classList.toggle("slide-left");
+		setTimeout(() => {
+			formSteps[currStep].classList.toggle("slide-left");
+			formSteps[currStep].classList.toggle("active");
+			currStep = (currStep + 1) % 3;
+			formSteps[currStep].classList.toggle("active");
+			formCounter[currStep].classList.toggle("active");
+		}, 600);
+	})
+);
 
-document.querySelectorAll(".prev")
-    .forEach(btn => btn.addEventListener("click", () => {
-        formSteps[currStep].classList.toggle("slide-right");
-        setTimeout(() => {
-            formSteps[currStep].classList.toggle("slide-right");
-            formSteps[currStep].classList.toggle("active");
-            formCounter[currStep].classList.toggle("active");
+document.querySelectorAll(".prev").forEach(btn =>
+	btn.addEventListener("click", () => {
+		formSteps[currStep].classList.toggle("slide-right");
+		setTimeout(() => {
+			formSteps[currStep].classList.toggle("slide-right");
+			formSteps[currStep].classList.toggle("active");
+			formCounter[currStep].classList.toggle("active");
 
-            currStep = (currStep - 1) % 3;
+			currStep = (currStep - 1) % 3;
 
-            formSteps[currStep].classList.toggle("active");
-        }, 600);
-    }));
-
+			formSteps[currStep].classList.toggle("active");
+		}, 600);
+	})
+);
